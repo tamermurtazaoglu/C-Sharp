2b784ff703916891de4c84fc5575894e8912e21a
Tamer Murtazaoğlu <tamermurtazaoglu@gmail.com>
2021-01-21 00:33:46 +0300

    01/20 end of work

---
+++ Collections/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "Tamer", "Metin", "Ahmet", "Üveys" };
+
+            List<string> isimler = new List<string> { "Tamer", "Metin", "Ahmet", "Üveys" };
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            Console.WriteLine(isimler[2]);
+            Console.WriteLine(isimler[3]);
+            isimler.Add("Recep");
+            Console.WriteLine(isimler[4]);
+            Console.WriteLine(isimler[0]);
+
+        }
+    }
+}
