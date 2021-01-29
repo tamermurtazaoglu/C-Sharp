3c32b7f15cab6f4a740690a97b2bf712779dd15b
Tamer Murtazaoğlu <tamermurtazaoglu@gmail.com>
2021-01-29 20:01:05 +0300

    01/29 end of work

---
+++ OOP2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Tamer";
+            musteri1.Soyadi = "Murtazaoğlu";
+            musteri1.TcNo = "12345678910";
+
+            musteri1.Id = 2;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Tamer";
+            musteri1.Soyadi = "Murtazaoğlu";
+            musteri1.TcNo = "12345678910";
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IhtiyacKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            TasitKrediManager tasitKrediManager = new TasitKrediManager();
+            KonutKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+
+            List<ILoggerService> loggerServices = new List<ILoggerService> { new FileLoggerService() , new SmsLoggerService()};
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(new EsnafKredisiManager(), loggerServices);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() { ihtiyacKrediManager, tasitKrediManager };
+
+
+        }
+    }
+}
